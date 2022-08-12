d566ace22f3ef9ea6938decbad74292b4f22a79a
alexxxstep <astep2004@gmail.com>
2022-08-13 00:23:30 +0300

    Add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,44 @@
 'use strict';
+
+// select header
+const header = document.querySelector('.header__container');
+
+/* =============== SHOW MENU =============== */
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+/* ===== MENU SHOW ===== */
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+
+/* ===== MENU HIDDEN ===== */
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 40) {
+    header.classList.add('active');
+  } else {
+    header.classList.remove('active');
+  }
+});
+
+/* =============== REMOVE MENU MOBILE =============== */
+
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
